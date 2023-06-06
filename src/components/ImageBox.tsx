b57916cf47ff3400ca79d0651d0eeed6c8f7c411
dylan h <baolong281@gmail.com>
2023-06-05 21:49:49 -0500

    paste images drop images text box

---
+++ src/components/ImageBox.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState, ClipboardEvent, ChangeEvent } from "react";
+
+interface ImageBoxProps {
+  setSelectedImage: React.Dispatch<React.SetStateAction<string | null>>;
+  selectedImage: string | null;
+}
+
+//box to upload images
+const ImageBox: React.FC<ImageBoxProps> = ({
+  setSelectedImage,
+  selectedImage,
+}): JSX.Element => {
+  const hiddenFileInput = React.useRef<HTMLInputElement>(null);
+
+  const handlePasteImage = (event: ClipboardEvent) => {
+    if (!event.clipboardData) return; // If clipboard data is valid
+
+    const pasteEvent = event.clipboardData;
+    const file = Array.from(pasteEvent.items).find(
+      (item) => item.type.indexOf("image") !== -1
+    );
+
+    if (file) {
+      const fileBlob = file.getAsFile();
+      if (fileBlob) {
+        const imageURL = URL.createObjectURL(fileBlob);
+        setSelectedImage(imageURL);
+      }
+    }
+  };
+
+  const handleUploadImage = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.[0]) return; // If file is valid
+
+    const file = event.target.files?.[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setSelectedImage(reader.result as string);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleClick = () => {
+    hiddenFileInput.current?.click();
+  };
+
+  const clearImage = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <div
+      className="border-2 border-slate-800 border-c rounded-lg flex justify-center p-2 h-1/3 w-full font-bold"
+      onPaste={handlePasteImage}
+    >
+      {selectedImage ? (
+        <div className="">
+          <button
+            onClick={clearImage}
+            className="bg-blue-600 rounded-full text-white p-2 absolute top-2 left-2 m-6"
+          >
+            Clear image
+          </button>
+          <img
+            src={selectedImage}
+            alt="Pasted"
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+          />
+        </div>
+      ) : (
+        <div className=" grid grid-cols-1 align-middle max mt-8">
+          <button
+            onClick={handleClick}
+            className="bg-blue-600 rounded-full max-h-14 px-6 text-white"
+          >
+            Upload image
+          </button>
+          <input
+            type="file"
+            accept="image/"
+            onChange={handleUploadImage}
+            ref={hiddenFileInput}
+            style={{ display: "none" }}
+          />
+          <div className="flex align-middle justify-center font-semibold">
+            Or drop / paste
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageBox;
