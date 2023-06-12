177aa67547580effcd850290f4fafe57317cb0cd
dylan h <baolong281@gmail.com>
2023-06-12 12:30:14 -0500

    dataset tokenizer

--- model/data/dataset.py
+++ model/data/dataset.py
@@ -3,6 +3,9 @@ import pandas as pd
 from torchvision import transforms
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+from transformers import PreTrainedTokenizerFast
+from torch.nn.utils.rnn import pad_sequence
+import os
 
 
 class ImagesDataset(Dataset):
@@ -15,9 +18,11 @@ class ImagesDataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        image_path = self.image_paths[index]
+        image_path = self.image_paths.iloc[index]
+
         image = Image.open(image_path)
-        formula = self.formulas[index]
+
+        formula = self.formulas.iloc[index]
 
         if self.transform:
             image = self.transform(image)
@@ -26,15 +31,44 @@ class ImagesDataset(Dataset):
 
 
 class Im2LatexDataset:
-    def __init__(self, batch_size=32, device=torch.device("cpu"), path_to_data="./"):
+    def __init__(
+        self,
+        block_size=216,
+        batch_size=32,
+        device=torch.device("cpu"),
+        path_to_data="./",
+        tokenizer="./tokenizer.json",
+    ):
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer)
+        self.tokenizer.pad_token = "[PAD]"
         self.path_to_data = path_to_data
         self.batch_size = batch_size
+        self.block_size = block_size
         dfs = (train_df, val_df, test_df) = self.load_dataframes()
         train_dataset, val_dataset, test_dataset = self.load_datasets(*dfs)
 
-        self.train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        self.val = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-        self.test = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+        def collate_fn(batch):
+            images, equations = zip(*batch)
+            tokens = self.tokenizer.batch_encode_plus(
+                equations,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt",
+                return_token_type_ids=False,
+                max_length=self.block_size,
+                pad_to_max_length=True,
+            )
+            return images, tokens
+
+        self.train = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn
+        )
+        self.val = DataLoader(
+            val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn
+        )
+        self.test = DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn
+        )
 
     def load_datasets(
         self, train_df, val_df, test_df
@@ -42,19 +76,9 @@ class Im2LatexDataset:
         transform = transforms.Compose(
             [
                 transforms.Resize((224, 600)),  # Resize to a specific size
-                transforms.Grayscale(num_output_channels=3),
+                transforms.Grayscale(num_output_channels=1),
                 transforms.ToTensor(),  # Convert to tensor
             ]
-        )
-
-        train_dataset = ImagesDataset(
-            train_df["image"], train_df["formula"], transform=transform
-        )
-        val_dataset = ImagesDataset(
-            val_df["image"], val_df["formula"], transform=transform
-        )
-        test_dataset = ImagesDataset(
-            test_df["image"], test_df["formula"], transform=transform
         )
 
         train_dataset = ImagesDataset(
@@ -70,20 +94,46 @@ class Im2LatexDataset:
         return train_dataset, val_dataset, test_dataset
 
     def load_dataframes(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        train_handwritten_df = pd.read_csv(self.path_to_data + "train_handwritten.csv")
-        val_handwritten_df = pd.read_csv(self.path_to_data + "val_handwritten.csv")
+        train_handwritten_df = pd.read_csv(
+            self.path_to_data + "train_handwritten.csv"
+        ).dropna(inplace=True)
+        val_handwritten_df = pd.read_csv(
+            self.path_to_data + "val_handwritten.csv"
+        ).dropna(inplace=True)
         train_df = pd.read_csv(self.path_to_data + "im2latex_train.csv")
         val_df = pd.read_csv(self.path_to_data + "im2latex_validate.csv")
         test_df = pd.read_csv(self.path_to_data + "im2latex_test.csv")
         dataframes = [train_df, val_df, test_df]
 
-        for df in dataframes:
-            df["image"] = df["image"].map(lambda x: self.fix_path(x))
+        def fix_path(path):
+            return self.path_to_data + "images/" + path
 
-        train_df = pd.concat([train_df, train_handwritten_df])
+        for df in dataframes:
+            df["image"] = df["image"].map(lambda x: fix_path(x))
+
+        train_df = pd.concat(
+            [train_df, train_handwritten_df]
+        )  # combine handwritten and non handwritten dataframes
         val_df = pd.concat([val_df, val_handwritten_df])
 
         return train_df, val_df, test_df
 
-    def fix_path(self, path):
-        return self.path_to_data + "images/" + path
+    def generate_tokenizer(equations, output, vocab_size):
+        from tokenizers import Tokenizer, pre_tokenizers
+        from tokenizers.models import BPE
+        from tokenizers.trainers import BpeTrainer
+
+        tokenizer = Tokenizer(BPE())
+        tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+        trainer = BpeTrainer(
+            special_tokens=[
+                "[PAD]",
+                "[BOS]",
+                "[EOS]",
+            ],
+            vocab_size=vocab_size,
+            show_progress=True,
+        )
+        tokenizer.pad_token = "[PAD]"
+        tokenizer.train([equations], trainer)
+        tokenizer.save(path=output, pretty=False)
