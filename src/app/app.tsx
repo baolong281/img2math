7fa77da5d03f8401a022f5ee5755179779ade72e
dylan h <baolong281@gmail.com>
2023-06-14 19:49:39 -0500

    mnist inference in browser with wasm

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,29 @@
 import ImageBox from "components/ImageBox";
 import InputBox from "components/InputBox";
 import MathBox from "components/MathBox";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as onnx from "onnxruntime-web";
 
 const App = (): JSX.Element => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [tex, setTex] = useState<string>("");
+
+  useEffect(() => {
+    const inference = async () => {
+      const session = await onnx.InferenceSession.create("./mnist.onnx", {
+        executionProviders: ["wasm"],
+      });
+      const input = new onnx.Tensor(
+        "float32",
+        new Float32Array(28 * 28),
+        [1, 1, 28, 28]
+      );
+      const feeds = { "input.1": input };
+      const results = await session.run(feeds);
+      return results["241"].data;
+    };
+    console.log(inference());
+  }, []);
 
   return (
     <div className="flex flex-col w-[48rem] h-[38rem] p-4 gap-4 overflow-hidden font-sans">
