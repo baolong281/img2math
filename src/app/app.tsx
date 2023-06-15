e6465540a5280ee4d432f63a1c686ef121de0b5c
dylan h <baolong281@gmail.com>
2023-06-15 13:19:20 -0500

    refactor: inference module

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,69 +1,36 @@
 import ImageBox from "components/ImageBox";
 import InputBox from "components/InputBox";
 import MathBox from "components/MathBox";
+import { getImageTensor } from "../utils/inference";
 import React, { useEffect, useState } from "react";
 import * as onnx from "onnxruntime-web";
-import Image from "image-js";
-import Matrix from "ml-matrix";
+
+const createSession = async () => {
+  const session = await onnx.InferenceSession.create("./mnist.onnx", {
+    executionProviders: ["wasm"],
+  });
+  return session;
+};
+
+let modelPromise = createSession();
 
 const App = (): JSX.Element => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [tex, setTex] = useState<string>("");
 
-  const getImageTensor = async (path: string | null) => {
-    if (!path) return;
-    const img = await Image.load(path);
-    const resized = img.grey().resize({ height: 28, width: 28 });
-    const normalizedImage = normalizeImage(resized);
-    return imageDataToTensor(normalizedImage);
-  };
-
-  const imageDataToTensor = (imageMatrix: Matrix): onnx.Tensor => {
-    const height = imageMatrix.rows;
-    const width = imageMatrix.columns;
-    const imageBuffer = new Float32Array(height * width);
-    const image1d = imageMatrix.to1DArray();
-
-    for (let i = 0; i < height * width; i++) {
-      imageBuffer[i] = image1d[i];
-    }
-
-    const imageTensor = new onnx.Tensor("float32", imageBuffer, [
-      1,
-      1,
-      height,
-      width,
-    ]);
-    return imageTensor;
-  };
-
-  const normalizeImage = (image: Image) => {
-    let mat: Matrix = image.getMatrix().div(255);
-    const mean = mat.mean();
-    const std = mat.standardDeviation();
-    return mat.subtract(mean).divide(std);
+  const inference = async () => {
+    const model = await modelPromise;
+    let input = await getImageTensor(selectedImage);
+    const feeds = { "input.1": input };
+    const results = await model.run(feeds);
+    console.log(results["241"].data);
+    return results["241"].data;
   };
 
   useEffect(() => {
-    getImageTensor(selectedImage);
+    if (!selectedImage) return;
+    const results = inference();
   }, [selectedImage]);
-
-  useEffect(() => {
-    const inference = async () => {
-      const session = await onnx.InferenceSession.create("./mnist.onnx", {
-        executionProviders: ["wasm"],
-      });
-      const input = new onnx.Tensor(
-        "float32",
-        new Float32Array(28 * 28),
-        [1, 1, 28, 28]
-      );
-      const feeds = { "input.1": input };
-      const results = await session.run(feeds);
-      return results["241"].data;
-    };
-    console.log(inference());
-  }, []);
 
   return (
     <div className="flex flex-col w-[48rem] h-[38rem] p-4 gap-4 overflow-hidden font-sans">
